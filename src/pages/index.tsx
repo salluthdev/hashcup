55f1126156ee7dd387d82004e46030b8582375be
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-09-01 17:01:32 +0700

    feat: add subtitle copywriting

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,14 @@ export default function Home() {
     <div className="relative py-6">
       {mounted ? (
         !isConnected ? (
-          <div className="max-w-md flex flex-col items-center gap-2 mx-auto">
+          <div className="max-w-md flex flex-col items-center gap-4 text-center mx-auto">
             <h1 className="text-2xl font-bold text-slate-800">
               Say Hello to HashCup 👋
             </h1>
+            <p>
+              Simple link to your crypto wallet. Share multiple assets overview
+              at once in a good way.
+            </p>
             <ConnectButton />
           </div>
         ) : (
