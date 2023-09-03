0507d5785bf9fcf2328fa54fade38816b2a307eb
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-09-03 16:09:48 +0700

    feat(index): get USDT and SDFT token balance

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,12 +7,17 @@ export default function Home() {
   const { address, isConnected } = useAccount();
   const mounted = useIsMounted();
 
-  const balance = useBalance({
+  const SDFTBalance = useBalance({
+    address: address,
+    token: "0x152b3942A33c78C72B576d28F75910A124Ca8181",
+  });
+
+  const USDTBalance = useBalance({
     address: address,
     token: "0xc2132D05D31c914a87C6611C10748AEb04B58e8F",
   });
 
-  console.log(balance.data);
+  console.log(USDTBalance);
 
   return (
     <div className="relative py-6">
@@ -22,7 +27,14 @@ export default function Home() {
         ) : (
           <div className="flex flex-col gap-4">
             <ConnectButton />
-            <p>{address}</p>
+            <p>Address: {address}</p>
+            <p>
+              SDFT: {SDFTBalance.data?.formatted} {SDFTBalance.data?.symbol}
+            </p>
+            <p>
+              USDT (polygon): {USDTBalance.data?.formatted}{" "}
+              {USDTBalance.data?.symbol}
+            </p>
           </div>
         )
       ) : null}
