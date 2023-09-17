5e4061a23de9d95c06a8174ee8e255b7bc66eb86
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-09-17 10:57:16 +0700

    feat(layouts/app): add rubik font from google fonts

--- src/components/common/layouts/app.jsx
+++ src/components/common/layouts/app.jsx
@@ -1,13 +1,15 @@
-import { ReactNode } from "react";
 import { Metadata } from "../metadata";
+import { Rubik } from "next/font/google";
+
+const rubik = Rubik({ subsets: ["latin"] });
 
 export default function AppLayout({ children }) {
   return (
-    <>
+    <div className={rubik.className}>
       <Metadata />
       <div className="max-w-3xl mx-auto">
         <main className="px-4">{children}</main>
       </div>
-    </>
+    </div>
   );
 }
