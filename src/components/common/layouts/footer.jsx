e9fbd8d75571c0e1a5c8314418125dc06eec0964
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-09-28 18:56:18 +0700

    feat(footer): add social icon and link

--- src/components/common/layouts/footer.jsx
+++ src/components/common/layouts/footer.jsx
@@ -1,18 +1,36 @@
 import Image from "next/image";
+import Link from "next/link";
+
+const CREATOR_SOCIAL = [
+  {
+    name: "instagram",
+    link: "https://www.instagram.com/salluthdev/",
+  },
+  {
+    name: "twitter",
+    link: "https://www.instagram.com/salluthdev/",
+  },
+  {
+    name: "linkedin",
+    link: "https://www.instagram.com/salluthdev/",
+  },
+];
 
 export default function Footer() {
   return (
-    <div className="w-full flex flex-col gap-4 items-center bg-very_pale_orange py-10 px-4">
+    <div className="w-full flex flex-col gap-5 items-center bg-very_pale_orange py-10 px-4">
       <p>Connect with The Creator</p>
-      <div className="flex items-center gap-2">
-        <div className="w-7 h-7 relative flex justify-center items-center bg-root_beer rounded-full  cursor-pointer">
-          <Image
-            src={"/svg/social/instagram.svg"}
-            width={16}
-            height={16}
-            alt=""
-          />
-        </div>
+      <div className="flex items-center gap-4">
+        {CREATOR_SOCIAL.map((social) => (
+          <Link key={social.name} href={social.link}>
+            <Image
+              src={`/svg/social/${social.name}.svg`}
+              width={20}
+              height={20}
+              alt=""
+            />
+          </Link>
+        ))}
       </div>
     </div>
   );
