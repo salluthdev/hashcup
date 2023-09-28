b51082f9fcb8a28e5c47b922ac9662153c5a4904
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-09-28 18:30:08 +0700

    feat(footer): add instagram icon

--- src/components/common/layouts/app.jsx
+++ src/components/common/layouts/app.jsx
@@ -7,12 +7,10 @@ const rubik = Rubik({ subsets: ["latin"] });
 export default function AppLayout({ children }) {
   return (
     <div
-      className={`min-h-screen flex flex-col bg-seashell ${rubik.className}`}
+      className={`min-h-screen flex flex-col text-root_beer bg-seashell ${rubik.className}`}
     >
       <Metadata />
-      <main className="w-full max-w-3xl flex-1 text-root_beer px-4 mx-auto">
-        {children}
-      </main>
+      <main className="w-full max-w-3xl flex-1 px-4 mx-auto">{children}</main>
       <Footer />
     </div>
   );
--- src/components/common/layouts/footer.jsx
+++ src/components/common/layouts/footer.jsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <div className="w-full flex flex-col items-center bg-very_pale_orange py-10 px-4">
+    <div className="w-full flex flex-col gap-4 items-center bg-very_pale_orange py-10 px-4">
       <p>Connect with The Creator</p>
+      <div className="flex items-center gap-2">
+        <div className="w-7 h-7 relative flex justify-center items-center bg-root_beer rounded-full  cursor-pointer">
+          <Image
+            src={"/svg/social/instagram.svg"}
+            width={16}
+            height={16}
+            alt=""
+          />
+        </div>
+      </div>
     </div>
   );
 }
