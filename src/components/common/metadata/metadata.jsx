dad44f02930f718fe3e0b4124daaccde68630e27
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-09-29 15:48:14 +0700

    feat(metadata): add meta keywords

--- src/components/common/metadata/metadata.jsx
+++ src/components/common/metadata/metadata.jsx
@@ -12,6 +12,10 @@ export default function Metadata({ title }) {
         name="description"
         content="Your clean and transparent crypto assets manager. Easily track your Ethereum, Binance Smart Chain (BSC), and Polygon network assets in one cup. Explore the smooth Pacoca alternative with HashCup and dive into our open-source code on GitHub."
       />
+      <meta
+        name="keywords"
+        content="multiple assets manager, multichain assets management, crypto assets manager, pacoca alternative, ethereum assets manager, bsc assets manager, polygon assets manager, cryptocurrency assets tracking"
+      />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/img/hashcup-logo-dummy.png " />
     </Head>
