2ade71d277176464ea22e8315c23e79b93904703
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-09-29 15:49:32 +0700

    feat(metadata): add meta author

--- src/components/common/metadata/metadata.jsx
+++ src/components/common/metadata/metadata.jsx
@@ -16,6 +16,7 @@ export default function Metadata({ title }) {
         name="keywords"
         content="multiple assets manager, multichain assets management, crypto assets manager, pacoca alternative, ethereum assets manager, bsc assets manager, polygon assets manager, cryptocurrency assets tracking"
       />
+      <meta name="author" content="salluthdev" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/img/hashcup-logo-dummy.png " />
     </Head>
