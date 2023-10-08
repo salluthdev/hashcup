9a95443edde7848f4941a76117bd3cae39ed77e6
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-10-08 18:12:54 +0700

    feat: hide header when user not connected

--- src/components/common/layouts/app.jsx
+++ src/components/common/layouts/app.jsx
@@ -1,16 +1,21 @@
+import { useAccount } from "wagmi";
 import { Footer, Header } from ".";
 import { Metadata } from "../metadata";
 import { Rubik } from "next/font/google";
+import { useIsMounted } from "@/hooks";
 
 const rubik = Rubik({ subsets: ["latin"] });
 
 export default function AppLayout({ children }) {
+  const { isConnected } = useAccount();
+  const mounted = useIsMounted();
+
   return (
     <div
       className={`min-h-screen flex flex-col text-root_beer bg-seashell ${rubik.className}`}
     >
       <Metadata />
-      <Header />
+      {mounted ? isConnected ? <Header /> : null : null}
       <main className="flex-1 py-10">{children}</main>
       <Footer />
     </div>
