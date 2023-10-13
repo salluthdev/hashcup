347890d92c3328dc01552ccf153b531f1733175c
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-10-13 22:42:24 +0700

    refactor(modal-token-detail): create reusable component token detail row

--- src/components/common/modal/modal-token-detail.jsx
+++ src/components/common/modal/modal-token-detail.jsx
@@ -4,47 +4,43 @@ import Image from "next/image";
 import { toast } from "react-toastify";
 
 export function ModalTokenDetail({ setModal, selectedTokenDetail }) {
+  const tokenDetailRow = (label, value, clickAction) => (
+    <div className="flex justify-between items-center gap-4 text-sm">
+      <p>{label}:</p>
+      {clickAction ? (
+        <div
+          className="group flex items-center gap-1 cursor-pointer"
+          onClick={clickAction}
+        >
+          <p className="truncate">{value}</p>
+          <Image
+            src={"/svg/icon-copy.svg"}
+            width={12}
+            height={12}
+            alt=""
+            className="group-hover:scale-105 group-active:scale-95 transition"
+          />
+        </div>
+      ) : (
+        <p>{value || "-"}</p>
+      )}
+    </div>
+  );
+
   return (
     <Modal setModal={setModal} title={`About ${selectedTokenDetail.symbol}`}>
-      <div className="flex justify-between items-center gap-4 text-sm">
-        <p>Name:</p>
-        <p>{selectedTokenDetail.name ? selectedTokenDetail.name : "-"}</p>
-      </div>
-      <div className="flex justify-between items-center gap-4 text-sm">
-        <p>Symbol:</p>
-        <p>{selectedTokenDetail.symbol ? selectedTokenDetail.symbol : "-"}</p>
-      </div>
-      <div className="flex justify-between items-center gap-4 text-sm">
-        <p>Price:</p>
-        <p>
-          {USDFormat(
-            selectedTokenDetail.price ? selectedTokenDetail.price : "-"
-          )}
-        </p>
-      </div>
-      {selectedTokenDetail.token_address && (
-        <div className="flex justify-between items-center gap-4 text-sm">
-          <p>Address:</p>
-          <div
-            className="group flex items-center gap-1 cursor-pointer"
-            onClick={() => {
-              navigator.clipboard.writeText(selectedTokenDetail.token_address);
-              toast.success("Address Copied");
-            }}
-          >
-            <p className="truncate">
-              {shortenAddress(selectedTokenDetail.token_address)}
-            </p>
-            <Image
-              src={"/svg/icon-copy.svg"}
-              width={12}
-              height={12}
-              alt=""
-              className="group-hover:scale-105 group-active:scale-95 transition"
-            />
-          </div>
-        </div>
-      )}
+      {tokenDetailRow("Name", selectedTokenDetail.name)}
+      {tokenDetailRow("Symbol", selectedTokenDetail.symbol)}
+      {tokenDetailRow("Price", USDFormat(selectedTokenDetail.price))}
+      {selectedTokenDetail.token_address &&
+        tokenDetailRow(
+          "Address",
+          shortenAddress(selectedTokenDetail.token_address),
+          () => {
+            navigator.clipboard.writeText(selectedTokenDetail.token_address);
+            toast.success("Address Copied");
+          }
+        )}
     </Modal>
   );
 }
