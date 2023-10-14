71def2c90a01c943bd4653f1f114bdeeccce96cb
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-10-14 20:33:58 +0700

    feat(dashboard): add copywriting info if no asset detected

--- src/components/pages/home/dashboard.jsx
+++ src/components/pages/home/dashboard.jsx
@@ -18,12 +18,15 @@ export default function Dashboard() {
   const { address } = useAccount();
   const [tokenList, setTokenList] = useState([]);
   const [totalNetWorth, setTotalNetWorth] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
   const [selectedTokenDetail, setSelectedTokenDetail] = useState([]);
   const [modal, setModal] = useState("");
 
   console.log(tokenList);
 
   const getTokenDatas = async () => {
+    setIsLoading(true);
+
     try {
       await startMoralis();
 
@@ -41,6 +44,7 @@ export default function Dashboard() {
       // Short tokenlist from higher total balance to lower
       const sortedTokenList = sortTokenList(flattenedBalances);
       setTokenList(sortedTokenList);
+      setIsLoading(false);
 
       // Calculate total net worth
       const netWorth = calculateTotalNetWorth(sortedTokenList);
@@ -62,51 +66,56 @@ export default function Dashboard() {
           <p className="text-sm text-pastel_brown">(Net Worth)</p>
         </div>
         <div className="flex flex-col">
-          {tokenList.map((token, index) => (
-            <div
-              key={index}
-              className="group flex items-center gap-4 hover:bg-linen transition rounded-lg py-2 px-6 -mx-6 cursor-pointer"
-              onClick={() => {
-                setSelectedTokenDetail(token);
-                setModal("modal-token-detail");
-              }}
-            >
-              <Image
-                src={`/img/token/${token?.network}/${
-                  token?.token_address ? token?.token_address : "native"
-                }.png`}
-                width={24}
-                height={24}
-                alt=""
-                className="group-hover:scale-105 group-active:scale-95 transition rounded-full"
-              />
-              <div className="flex-1 flex flex-col gap-2">
-                <div className="flex justify-between items-center gap-4">
-                  <p className="font-medium">{token?.symbol}</p>
-                  <div className="flex items-center gap-2">
-                    <p className="font-semibold">
-                      {USDFormat(
-                        (token?.balance / 10 ** token?.decimals) * token?.price
-                      )}
+          {tokenList.length > 0 ? (
+            tokenList.map((token, index) => (
+              <div
+                key={index}
+                className="group flex items-center gap-4 hover:bg-linen transition rounded-lg py-2 px-6 -mx-6 cursor-pointer"
+                onClick={() => {
+                  setSelectedTokenDetail(token);
+                  setModal("modal-token-detail");
+                }}
+              >
+                <Image
+                  src={`/img/token/${token?.network}/${
+                    token?.token_address ? token?.token_address : "native"
+                  }.png`}
+                  width={24}
+                  height={24}
+                  alt=""
+                  className="group-hover:scale-105 group-active:scale-95 transition rounded-full"
+                />
+                <div className="flex-1 flex flex-col gap-2">
+                  <div className="flex justify-between items-center gap-4">
+                    <p className="font-medium">{token?.symbol}</p>
+                    <div className="flex items-center gap-2">
+                      <p className="font-semibold">
+                        {USDFormat(
+                          (token?.balance / 10 ** token?.decimals) *
+                            token?.price
+                        )}
+                      </p>
+                      <Image
+                        src={`/svg/network/${token?.network}.svg`}
+                        width={16}
+                        height={16}
+                        alt=""
+                      />
+                    </div>
+                  </div>
+                  <div className="flex justify-between items-center gap-4 text-pastel_brown">
+                    <p>{USDFormat(token?.price ? token?.price : "-")}</p>
+                    <p>
+                      {NumberFormat(token?.balance / 10 ** token?.decimals)}{" "}
+                      {token?.symbol}
                     </p>
-                    <Image
-                      src={`/svg/network/${token?.network}.svg`}
-                      width={16}
-                      height={16}
-                      alt=""
-                    />
                   </div>
                 </div>
-                <div className="flex justify-between items-center gap-4 text-pastel_brown">
-                  <p>{USDFormat(token?.price ? token?.price : "-")}</p>
-                  <p>
-                    {NumberFormat(token?.balance / 10 ** token?.decimals)}{" "}
-                    {token?.symbol}
-                  </p>
-                </div>
               </div>
-            </div>
-          ))}
+            ))
+          ) : !isLoading ? (
+            <p>Oops! No asset detected in this wallet address :(</p>
+          ) : null}
         </div>
       </div>
       {modal === "modal-token-detail" && (
