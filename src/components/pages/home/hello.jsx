c24fa7e26b10d83f539f1d573f94e624085c7255
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-10-15 23:31:26 +0700

    feat(hello): add paste address option

--- src/components/pages/home/hello.jsx
+++ src/components/pages/home/hello.jsx
@@ -9,22 +9,38 @@ export default function Hello() {
   const { openConnectModal } = useConnectModal();
 
   return (
-    <div className="max-w-sm flex flex-col items-center gap-4 text-center py-10 px-4 mx-auto">
-      <Image
-        src={"/img/hashcup-logo.png"}
-        width={80}
-        height={80}
-        alt="hashcup-logo"
-      />
-      <h1 className="text-2xl font-bold">Say Hello to HashCup 👋</h1>
-      <p>
-        Multichain assets manager. Track your wallet overview at once in one
-        cup.
-      </p>
+    <div className="max-w-sm flex flex-col items-center gap-7 text-center py-10 px-4 mx-auto">
+      <div className="flex flex-col items-center gap-4">
+        <Image
+          src={"/img/hashcup-logo.png"}
+          width={80}
+          height={80}
+          alt="hashcup-logo"
+        />
+        <h1 className="text-2xl font-bold">Say Hello to HashCup 👋</h1>
+        <p>
+          Multichain assets manager. Track your wallet overview at once in one
+          cup.
+        </p>
+      </div>
       <Button onClick={openConnectModal} withoutHoverAnim>
         Connect Wallet
       </Button>
-      <p className="text-sm mt-4">
+      <div className="w-full flex items-center gap-5">
+        <hr className="w-full h-px bg-gray-300 border-0" />
+        <p className="text-pastel_brown">or</p>
+        <hr className="w-full h-px bg-gray-300 border-0" />
+      </div>
+      <div className="w-full flex">
+        <input
+          placeholder="Paste your wallet address"
+          className="flex-1 text-root_beer placeholder:text-pastel_brown border-2 border-very_pale_orange rounded-l-lg outline-none py-4 p-5"
+        />
+        <button className="font-medium bg-very_pale_orange rounded-r-lg py-4 px-5">
+          Go
+        </button>
+      </div>
+      <p className="text-sm">
         Don&apos;t have an address?{" "}
         <span
           className="font-medium underline cursor-pointer"
