28ba6198a9280cb1d7e6fbdb17b8227baaa710f1
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-10-15 19:20:32 +0700

    feat(_document): add google analytics script

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,23 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-6FJHQFYY8Z"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-6FJHQFYY8Z');
+              `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
