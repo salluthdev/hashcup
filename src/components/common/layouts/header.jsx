cb600d24891763c607f6d15eb79d12baa5633bf9
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-10-15 15:53:18 +0700

    feat(header): add link to github repo

--- src/components/common/layouts/header.jsx
+++ src/components/common/layouts/header.jsx
@@ -14,10 +14,14 @@ export default function Header() {
         />
       </Link>
       <div className="flex items-center gap-4">
-        <div className="flex items-center gap-1 bg-white rounded-lg shadow-[0_4px_12px_rgba(0,0,0,0.1)] py-2 px-3 hover:scale-105 active:scale-95 transition cursor-pointer">
+        <Link
+          href={"https://github.com/salluthdev/hashcup"}
+          target="_blank"
+          className="flex items-center gap-1 bg-white rounded-lg shadow-[0_4px_12px_rgba(0,0,0,0.1)] py-2 px-3 hover:scale-105 active:scale-95 transition cursor-pointer"
+        >
           <Image src={"/svg/icon-star.svg"} width={16} height={16} alt="" />
           <p className="font-medium text-brandeis_blue">21</p>
-        </div>
+        </Link>
         <ConnectButton showBalance={false} chainStatus={"none"} />
       </div>
     </div>
