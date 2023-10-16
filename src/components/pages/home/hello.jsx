860ff21b8cccf03fbed26c62fbbca6300466f7d5
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-10-16 20:54:36 +0700

    feat(hello): check valid eth address

--- src/components/pages/home/hello.jsx
+++ src/components/pages/home/hello.jsx
@@ -3,11 +3,15 @@ import { TrackedAddressContext } from "@/context";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 import Image from "next/image";
 import { useContext, useState } from "react";
+import { toast } from "react-toastify";
+import Web3 from "web3";
 
 export default function Hello() {
   const { setTrackedAddress } = useContext(TrackedAddressContext);
   const { openConnectModal } = useConnectModal();
   const [address, setAddress] = useState("");
+
+  console.log(address.length);
 
   return (
     <div className="max-w-sm flex flex-col items-center gap-7 text-center py-10 px-4 mx-auto">
@@ -32,7 +36,14 @@ export default function Hello() {
         <p className="text-pastel_brown">or</p>
         <hr className="w-full h-px bg-gray-300 border-0" />
       </div>
-      <form className="w-full flex" onSubmit={() => setTrackedAddress(address)}>
+      <form
+        className="w-full flex"
+        onSubmit={(e) =>
+          Web3.utils.isAddress(address)
+            ? setTrackedAddress(address)
+            : (e.preventDefault(), toast.error("Invalid Address 🤔"))
+        }
+      >
         <input
           placeholder="Paste your wallet address"
           className="flex-1 text-root_beer placeholder:text-pastel_brown border-2 border-very_pale_orange rounded-l-lg outline-none py-4 p-5"
