a35fa459017e275950313ed3c4145c6852c31427
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-11-12 13:04:54 +0700

    feat(modal-token-detail): add blockchain explorer

--- src/components/common/modal/modal-token-detail.jsx
+++ src/components/common/modal/modal-token-detail.jsx
@@ -5,7 +5,14 @@ import { toast } from "react-toastify";
 import Link from "next/link";
 
 export function ModalTokenDetail({ setModal, selectedTokenDetail }) {
-  console.log(selectedTokenDetail);
+  const block_explorer =
+    selectedTokenDetail.network === "eth"
+      ? "etherscan"
+      : selectedTokenDetail.network === "bsc"
+      ? "bscscan"
+      : selectedTokenDetail.network === "polygon"
+      ? "polygonscan"
+      : "";
 
   const tokenDetailRow = (label, value, clickAction) => (
     <div className="flex justify-between items-center gap-4 text-sm">
@@ -44,31 +51,36 @@ export function ModalTokenDetail({ setModal, selectedTokenDetail }) {
             toast.success("Address Copied 🥳🎉");
           }
         )}
-      <div className="flex justify-center items-center gap-3 mt-2">
-        <Link href={"/"}>
+      {selectedTokenDetail.token_address && (
+        <div className="flex justify-center items-center gap-3 mt-2">
+          <Link
+            href={`https://${block_explorer}.com/token/${selectedTokenDetail.token_address}`}
+            target="_blank"
+          >
+            <Image
+              src={"/svg/icon-globe.svg"}
+              width={16}
+              height={16}
+              alt=""
+              className="hover:scale-105 active:scale-95 transition"
+            />
+          </Link>
           <Image
-            src={"/svg/icon-globe.svg"}
+            src={"/img/icon-metamask.png"}
             width={16}
             height={16}
             alt=""
-            className="hover:scale-105 active:scale-95 transition"
+            className="hover:scale-105 active:scale-95 transition cursor-pointer"
           />
-        </Link>
-        <Image
-          src={"/img/icon-metamask.png"}
-          width={16}
-          height={16}
-          alt=""
-          className="hover:scale-105 active:scale-95 transition cursor-pointer"
-        />
-        <Image
-          src={"/svg/icon-trustwallet.svg"}
-          width={16}
-          height={16}
-          alt=""
-          className="hover:scale-105 active:scale-95 transition cursor-pointer"
-        />
-      </div>
+          <Image
+            src={"/svg/icon-trustwallet.svg"}
+            width={16}
+            height={16}
+            alt=""
+            className="hover:scale-105 active:scale-95 transition cursor-pointer"
+          />
+        </div>
+      )}
     </Modal>
   );
 }
