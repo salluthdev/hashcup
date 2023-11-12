6b0928b89719a270339199143f1dbe535bbd5dc9
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-11-12 12:53:02 +0700

    feat(modal): add icon globe, metamask, and trustwallet

--- src/components/common/modal/modal-token-detail.jsx
+++ src/components/common/modal/modal-token-detail.jsx
@@ -2,8 +2,11 @@ import { USDFormat, shortenAddress } from "@/utils";
 import { Modal } from ".";
 import Image from "next/image";
 import { toast } from "react-toastify";
+import Link from "next/link";
 
 export function ModalTokenDetail({ setModal, selectedTokenDetail }) {
+  console.log(selectedTokenDetail);
+
   const tokenDetailRow = (label, value, clickAction) => (
     <div className="flex justify-between items-center gap-4 text-sm">
       <p>{label}:</p>
@@ -41,6 +44,31 @@ export function ModalTokenDetail({ setModal, selectedTokenDetail }) {
             toast.success("Address Copied 🥳🎉");
           }
         )}
+      <div className="flex justify-center items-center gap-3 mt-2">
+        <Link href={"/"}>
+          <Image
+            src={"/svg/icon-globe.svg"}
+            width={16}
+            height={16}
+            alt=""
+            className="hover:scale-105 active:scale-95 transition"
+          />
+        </Link>
+        <Image
+          src={"/img/icon-metamask.png"}
+          width={16}
+          height={16}
+          alt=""
+          className="hover:scale-105 active:scale-95 transition cursor-pointer"
+        />
+        <Image
+          src={"/svg/icon-trustwallet.svg"}
+          width={16}
+          height={16}
+          alt=""
+          className="hover:scale-105 active:scale-95 transition cursor-pointer"
+        />
+      </div>
     </Modal>
   );
 }
