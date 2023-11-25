8ecf3eb741e6d9e926cc099b0ee3c3eb78b7f3d9
Faisal L <83701344+salluthdev@users.noreply.github.com>
2023-11-25 12:51:03 +0700

    refactor(dashboard): add token-list component

--- src/components/pages/home/dashboard/index.jsx
+++ src/components/pages/home/dashboard/index.jsx
@@ -1,188 +1,27 @@
-import {
-  ModalTokenDetail,
-  ModalTokenTransfer,
-} from "@/components/common/modal";
 import { TrackedAddressContext } from "@/context";
-import {
-  NumberFormat,
-  USDFormat,
-  calculateTotalNetWorth,
-  getNativeTokenData,
-  getNonNativeTokenData,
-  sortTokenList,
-  startMoralis,
-} from "@/utils";
-import Image from "next/image";
-import { useContext, useEffect, useState } from "react";
-import { toast } from "react-toastify";
+import { useContext, useState } from "react";
 import { useAccount } from "wagmi";
 import Overview from "./overview";
-
-const chainIds = ["0x1", "0x38", "0x89"];
+import TokenList from "./token-list";
 
 export default function Dashboard() {
   const { address } = useAccount();
   const { trackedAddress } = useContext(TrackedAddressContext);
   const [tokenList, setTokenList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [selectedTokenDetail, setSelectedTokenDetail] = useState([]);
-  const [modal, setModal] = useState("");
-  const [tokenImageError, setTokenImageError] = useState([]);
-
-  // const getTokenDatas = async () => {
-  //   setIsLoading(true);
-  //   setTokenList([]);
-
-  //   try {
-  //     await startMoralis();
-
-  //     // Get native and non-native token data
-  //     const getTokenData = chainIds.map(async (chainId) => {
-  //       const nativeTokenData = await getNativeTokenData(
-  //         chainId,
-  //         address || trackedAddress
-  //       );
-  //       const tokenData = await getNonNativeTokenData(
-  //         chainId,
-  //         address || trackedAddress
-  //       );
-
-  //       return Promise.all([nativeTokenData, ...tokenData]);
-  //     });
-
-  //     const allBalances = await Promise.all(getTokenData);
-  //     const flattenedBalances = allBalances.flat();
-
-  //     // Short tokenlist from higher total balance to lower
-  //     const sortedTokenList = sortTokenList(flattenedBalances);
-  //     setTokenList(sortedTokenList);
-  //     setIsLoading(false);
-  //   } catch (error) {
-  //     console.log(error);
-  //     setIsLoading(false);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getTokenDatas();
-  // }, [address, trackedAddress]);
 
   return (
-    <>
-      <div className="w-full max-w-xl flex flex-col gap-6 px-4 pt-6 pb-10 mx-auto">
-        <Overview
-          tokenList={tokenList}
-          address={address}
-          trackedAddress={trackedAddress}
-        />
-        <div className="flex flex-col">
-          {tokenList.length > 0 ? (
-            tokenList.map((token, index) => (
-              <div
-                key={index}
-                className="flex justify-between items-center gap-4 hover:bg-linen transition rounded-lg py-2 px-6 -mx-6 cursor-pointer"
-                onClick={() => {
-                  setSelectedTokenDetail(token);
-                  setModal("modal-token-detail");
-                }}
-              >
-                <div className="relative">
-                  {!tokenImageError.includes(token?.token_address) ? (
-                    <Image
-                      src={`/img/token/${token?.network}/${
-                        token?.token_address ? token?.token_address : "native"
-                      }.png`}
-                      width={24}
-                      height={24}
-                      alt=""
-                      className="rounded-full"
-                      onError={() => {
-                        setTokenImageError((prevErrors) => [
-                          ...prevErrors,
-                          token?.token_address,
-                        ]);
-                      }}
-                    />
-                  ) : (
-                    <div className="w-6 h-6 flex justify-center items-center bg-root_beer rounded-full">
-                      <span className="text-xs font-medium text-white">
-                        {token?.symbol[0].toUpperCase()}
-                      </span>
-                    </div>
-                  )}
-                </div>
-                <div className="flex-1 flex flex-col gap-2">
-                  <div className="flex justify-between items-center gap-4">
-                    <p className="font-medium">{token?.symbol}</p>
-                    <div className="flex items-center gap-2">
-                      <p className="font-semibold">
-                        {USDFormat(
-                          (token?.balance / 10 ** token?.decimals) *
-                            token?.price
-                        )}
-                      </p>
-                      <Image
-                        src={`/svg/network/${token?.network}.svg`}
-                        width={16}
-                        height={16}
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                  <div className="flex justify-between items-center gap-4 text-pastel_brown">
-                    <p>{USDFormat(token?.price ? token?.price : "-")}</p>
-                    <p>
-                      {NumberFormat(token?.balance / 10 ** token?.decimals)}{" "}
-                      {token?.symbol}
-                    </p>
-                  </div>
-                </div>
-                <div
-                  className="group bg-linen hover:bg-root_beer rounded-lg p-2 transition"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    setSelectedTokenDetail(token);
-                    setModal("modal-token-transfer");
-                    toast.info("Coming Soon 🐱‍💻");
-                  }}
-                >
-                  <Image
-                    src={"/svg/icon-send.svg"}
-                    width={16}
-                    height={16}
-                    alt=""
-                    className="group-hover:hidden"
-                  />
-                  <Image
-                    src={"/svg/icon-send-light.svg"}
-                    width={16}
-                    height={16}
-                    alt=""
-                    className="hidden group-hover:block"
-                  />
-                </div>
-              </div>
-            ))
-          ) : !isLoading ? (
-            <p>Oops! Something error, please contact the developer :(</p>
-          ) : (
-            <p>Loading...</p>
-          )}
-        </div>
-      </div>
-      {modal === "modal-token-detail" ? (
-        <ModalTokenDetail
-          setModal={setModal}
-          selectedTokenDetail={selectedTokenDetail}
-        />
-      ) : (
-        modal === "modal-token-transfer" && (
-          <ModalTokenTransfer
-            setModal={setModal}
-            selectedTokenDetail={selectedTokenDetail}
-          />
-        )
-      )}
-    </>
+    <div className="w-full max-w-xl flex flex-col gap-6 px-4 pt-6 pb-10 mx-auto">
+      <Overview
+        tokenList={tokenList}
+        address={address}
+        trackedAddress={trackedAddress}
+      />
+      <TokenList
+        tokenList={tokenList}
+        setTokenList={setTokenList}
+        address={address}
+        trackedAddress={trackedAddress}
+      />
+    </div>
   );
 }
